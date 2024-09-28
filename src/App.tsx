c8864451b133023a3f42dc3025041bd78144c7b7
Gaurav Kumar <gk9848970@gmail.com>
2024-09-28 17:41:36 +0530

    Deduplication

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import {
   useQuery,
 } from "@tanstack/react-query";
 
-function Book() {
-  const { data } = useQuery({
-    queryKey: ["book"],
-    queryFn: () => Promise.resolve("Batman"),
+const useLuckyNumber = () => {
+  return useQuery({
+    queryKey: ["luckyNumber"],
+    queryFn: () => Promise.resolve(Math.random()),
   });
+};
+
+function LuckyNumber() {
+  const { data } = useLuckyNumber();
   return <h1>{data}</h1>;
 }
 
@@ -18,7 +22,16 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <h1>Hello world!</h1>
-      <Book />
+
+      {/* 
+        Deduplication
+        First resolves query places data in cache, 
+        And for others return from the cache only 
+      */}
+
+      <LuckyNumber />
+      <LuckyNumber />
+      <LuckyNumber />
     </QueryClientProvider>
   );
 }
