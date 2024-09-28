fed3cbec244599d7448eccc6e334623e48c71e6c
Gaurav Kumar <gk9848970@gmail.com>
2024-09-28 17:31:38 +0530

    Setting up query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+function Book() {
+  const { data } = useQuery({
+    queryKey: ["book"],
+    queryFn: () => Promise.resolve("Batman"),
+  });
+  return <h1>{data}</h1>;
+}
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1>Hello world!</h1>
+      <Book />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
