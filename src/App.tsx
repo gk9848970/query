30a3530292b4e940403d83d5b515a8363b4045b6
Gaurav Kumar <gk9848970@gmail.com>
2024-09-28 18:51:01 +0530

    Cleaned slate

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,10 @@ import {
   useQuery,
 } from "@tanstack/react-query";
 
-function MediaDevices() {
-  const { data, isError, isPending } = useQuery({
-    queryKey: ["mediaDevices"],
-    queryFn: async () => {
-      const devices = await navigator.mediaDevices.enumerateDevices();
-      return devices;
-    },
-  });
-
-  if (isPending) {
-    return <p>Loading...</p>;
-  }
-
-  if (isError) {
-    return <p>Error: {isError}</p>;
-  }
-
-  return (
-    <h1>
-      {data.map((device, i) => (
-        <p key={i}>{device.label || i} Device</p>
-      ))}
-    </h1>
-  );
-}
-
 const queryClient = new QueryClient();
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <MediaDevices />
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
 }
 
 export default App;
