decdd844782bbce9995bbf9a211b3d7830e52008
Gaurav Kumar <gk9848970@gmail.com>
2024-09-29 10:17:44 +0530

    Fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,48 @@ import {
   useQuery,
 } from "@tanstack/react-query";
 
-function MediaDevices() {
-  const { data, isError, isPending } = useQuery({
-    queryKey: ["mediaDevices"],
-    queryFn: async () => {
-      const devices = await navigator.mediaDevices.enumerateDevices();
-      return devices;
-    },
-  });
-
-  if (isPending) {
-    return <p>Loading...</p>;
-  }
-
-  if (isError) {
-    return <p>Error: {isError}</p>;
-  }
-
-  return (
-    <h1>
-      {data.map((device, i) => (
-        <p key={i}>{device.label || i} Device</p>
-      ))}
-    </h1>
-  );
-}
-
 const queryClient = new QueryClient();
 
+// Normal fetch
+const fetchRepos = async () => {
+  try {
+    const response = await fetch("https://api.github.com/orgs/TanStack/repos");
+    if (!response.ok) {
+      throw new Error(`Request failed with status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data as string;
+  } catch (error) {
+    console.error("Network error", error);
+  }
+};
+
+// Fetch inside a query
+
+const useReposQuery = () => {
+  return useQuery({
+    queryKey: ["repo"],
+    queryFn: async () => {
+      const response = await fetch(
+        "https://api.github.com/orgs/TanStack/repos"
+      );
+
+      if (!response.ok) {
+        throw new Error(`Request failed with status: ${response.status}`);
+      }
+
+      return (await response.json()) as string;
+    },
+  });
+};
+
+/*
+Thing to notice, Try and catch is gone
+Only for typescript: https://x.com/t3dotgg/status/1556539631323078657
+*/
+
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <MediaDevices />
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
 }
 
 export default App;
