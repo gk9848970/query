86dadb38de4dd0df851c4913f37d26e320fc8c49
Gaurav Kumar <gk9848970@gmail.com>
2024-09-30 18:16:34 +0530

    Dependant queries - I

--- src/App.tsx
+++ src/App.tsx
@@ -10,51 +10,78 @@ Refetch Interval: Can also be a function that returns boolean, Indicating if we 
 stop refetching. 
 */
 
-function useUuid() {
-  return useQuery({
-    queryKey: ["uuid"],
-    queryFn: async () => {
-      const response = await fetch(`https://uuid.rocks/json`);
+async function fetchMovie(title: string) {
+  const response = await fetch(
+    `https://ui.dev/api/courses/react-query/movies/${title}`
+  );
 
-      if (!response.ok) {
-        throw new Error("fetch failed");
-      }
+  if (!response.ok) {
+    throw new Error("fetch failed");
+  }
 
-      return response.json();
-    },
-    refetchInterval: 5000, // 3 seconds
-  });
+  return response.json();
 }
 
-const queryClient = new QueryClient();
+async function fetchDirector(id: string) {
+  const response = await fetch(
+    `https://ui.dev/api/courses/react-query/director/${id}`
+  );
 
-const UUIDComponent = () => {
-  const { data, status, fetchStatus, refetch, dataUpdatedAt } = useUuid();
+  if (!response.ok) {
+    throw new Error("fetch failed");
+  }
+
+  return response.json();
+}
+
+/*
+One way of dealing with dependant queries, is to combine both queries into one.
+But here deduplication is not possible, since we need to refetch both queries.
+
+[movie1, director1]
+[movie1, director2]
+
+Director data will be fetched twice, since we are not deduplicating the query.
+*/
+const useMovieAndDirector = (title: string) => {
+  return useQuery({
+    queryKey: ["movie", title],
+    queryFn: async () => {
+      const movie = await fetchMovie(title);
+      const director = await fetchDirector(movie.director);
+      return { movie, director };
+    },
+  });
+};
+
+const Component = ({ title }: { title: string }) => {
+  const { data, status } = useMovieAndDirector(title);
 
   if (status === "pending") {
-    return <div>...</div>;
+    return <div>Loading...</div>;
   }
 
   if (status === "error") {
-    return <div>Error fetching UUID</div>;
+    return <div>Error: </div>;
   }
 
   return (
-    <p>
-      <div>{data.uuid}</div>
-      <div>{new Date(dataUpdatedAt).toLocaleString()}</div>
-      <button onClick={() => refetch()}>Refetch</button>
-      <span>{fetchStatus === "fetching" ? "updating..." : null}</span>
-    </p>
+    <div>
+      <h1>{data.movie.title}</h1>
+      <h2>{data.director.name}</h2>
+    </div>
   );
 };
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       {/* The rest of your application */}
       <ReactQueryDevtools initialIsOpen={true} />
-      <UUIDComponent />
+      <Component title={"The Godfather"} />
+      <Component title={"The Godfather III"} />
     </QueryClientProvider>
   );
 }
