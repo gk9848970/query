db24532cce23c6cbad8cc2368dfe92be65be7979
Gaurav Kumar <gk9848970@gmail.com>
2024-10-01 14:39:21 +0530

    Parallel query III - A

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {
   QueryClient,
   QueryClientProvider,
+  useQueries,
   useQuery,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -23,41 +24,57 @@ Can have dynamic number of queries
 Here we have a single query which is a promise of all the queries
 It makes it all or none call, Resolves only when all the queries are resolved
 Fetching and caching cannot be done independently
+No collective info mechanism, Which failed Which resolved independently
+
+3. useQueries
+This combines the best of both worlds
+Dynamic number of queries can be supported as well
+Cache and fetching can be done independently and in parallel
+Collective info can be done by array methods on returned array
 */
 
 const Component = () => {
-  const promises = [fetchRepos(), fetchMembers()];
-  const reposAndMembers = useQuery({
-    queryKey: ["repos"],
-    queryFn: () => Promise.all(promises),
+  const [repos, members] = useQueries({
+    queries: [
+      {
+        queryKey: ["repos"],
+        queryFn: fetchRepos,
+      },
+      {
+        queryKey: ["members"],
+        queryFn: fetchMembers,
+      },
+    ],
   });
-
-  if (reposAndMembers.status !== "success") {
-    return <p>Loading...</p>;
-  }
-
-  const [repos, members] = reposAndMembers.data;
 
   return (
     <>
       <h1>TanStack Dashboard</h1>
       <h2>Repos</h2>
-
-      <ul>
-        {repos.map((repo) => (
-          <li key={repo.id}>{repo.name}</li>
-        ))}
-      </ul>
+      {repos.isPending ? <p>Loading repos...</p> : null}
+      {repos.isError ? <p>Error loading repos: {repos.error.message}</p> : null}
+      {repos.isSuccess ? (
+        <ul>
+          {repos.data.map((repo) => (
+            <li key={repo.id}>{repo.name}</li>
+          ))}
+        </ul>
+      ) : null}
 
       <hr />
 
       <h2>Members</h2>
-
-      <ul>
-        {members.map((member) => (
-          <li key={member.id}>{member.login}</li>
-        ))}
-      </ul>
+      {members.isPending ? <p>Loading members...</p> : null}
+      {members.isError ? (
+        <p>Error loading members: {members.error.message}</p>
+      ) : null}
+      {members.isSuccess ? (
+        <ul>
+          {members.data.map((member) => (
+            <li key={member.id}>{member.login}</li>
+          ))}
+        </ul>
+      ) : null}
     </>
   );
 };
